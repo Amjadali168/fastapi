0799b56c600f4cea0795a13506ab116b0acee246
Amjadali168 <amjadali1682002@gmail.com>
2024-12-24 13:53:35 +0530

    post method

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'Name':'Sarthak'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str]=None):
+    if published:
+      return {'data':f'{limit} published blog from db'}
+    else:
+      return {'data':f'{limit} blog from db'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blog'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+
+
+@app.get('/blog/{id}/comments')
+def show(id):
+    return {'data': {1,2}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+
+@app.post('/blog')
+def create_blog(request:Blog):
+    return request
+    return {'data':'blog is created'}
